c217ec8b037fde0c9768a50431f4292cfee4da71
Lloyd <lloyd@Lloyds-MacBook-Pro.local>
2019-11-03 21:34:02 +0000

    Code to update scores and change active player

--- app.js
+++ app.js
@@ -9,23 +9,22 @@ GAME RULES:
 
 */
 
-var scores, roundScores, activePlayer;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
-roundScores = 0;
-activePlayer = 1;
+roundScore = 0;
+activePlayer = 0;
 
 document.querySelector(".dice").style.display = "none";
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0'
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
 // Create and event listener to roll the dice when roll button is clicked
 document.querySelector(".btn-roll").addEventListener("click", function() {
-
-  //Create a Random number for dice roll  
+  //Create a Random number for dice roll
   var dice = Math.floor(Math.random() * 6) + 1;
 
   //Display the results of the button roll
@@ -33,5 +32,22 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
   diceDOM.style.display = "block";
   diceDOM.src = "dice-" + dice + ".png";
 
+  //Update the round score if the rolled number is not a 1
+  if (dice !== 1) {
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
 
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector(".dice").style.display = "none";
+
+
+  }
 });
